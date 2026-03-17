5cc68f50cca80fe44c4f77765d14498dfc680f6b
outercastl3 <outercastl3@proton.me>
2026-03-17 18:46:19 +0100

    finished simple script version

--- scenarios/ssh_bruteforce/automated_ban.py
+++ scenarios/ssh_bruteforce/automated_ban.py
@@ -5,8 +5,8 @@ def argument_parse():
     parser = argparse.ArgumentParser(
         prog='Automatic script to add IP-Address into pfctl ban tables',
         description='Script which SSHs into pfSense client and executes command of adding an IP-Address into firewall tables',
-        usage='automated_ban.py -l [PFSENSE_LOGIN] -p [PASSWORD] -ip [IP_ADDRESS]'
-        epiloge='Example: automated_ban.py -l root -p 12345 -ip 192.1.1.1'
+        usage='automated_ban.py -l [PFSENSE_LOGIN] -p [PASSWORD] -ip [IP_ADDRESS]',
+        epilog='Example: automated_ban.py -l root -p 12345 -ip 192.1.1.1'
         )
 
     parser.add_argument(
@@ -16,23 +16,48 @@ def argument_parse():
         )
 
     parser.add_argument(
+        "-gt_ip", "--gateway_ip",
+        required=True,
+        help="IP of the gateway"
+        )
+
+    parser.add_argument(
         "-p", "--password",
         required=True,
         help="Password for Login",
         )
 
     parser.add_argument(
-        "-ip", "--ip_address"
+        "-ip", "--ip_address",
         required=True,
         help="IP-Address to ban"
         )
     
     return parser.parse_args()
 
- def connection():
-    pass
+def connection(gateway_ip,login,password,ipaddr):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=f"{gateway_ip}", username=f"{login}", password=f"{password}")
+        stdin, stdout, stderr = client.exec_command(f"pfctl -t bruteforce -T add {ipaddr}")
 
- if __name__ == "__main__":
-     args = argument_parse()
-     connection(args.login,args.password,args.ip_address)
+        output = stdout.read().decode("utf-8")
+        errors = stderr.read().decode("utf-8")
+
+        print("Output:", output)
+        print("Errors:", errors)
+        
+        if not errors:
+            print(f"Successfully banned {ipaddr} on pfSense")
+
+    except Exception as e:
+        print(f"Connection failed: {e}")
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    args = argument_parse()
+    connection(args.gateway_ip,args.login,args.password,args.ip_address)
 
