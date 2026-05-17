aaa29255588ab00a70d9009081418c81e82ff997
outercastl3 <outercastl3@proton.me>
2026-05-17 16:24:08 +0200

    several small changes

--- scenarios/dmz-lab/attacker_protocols/scripts/bruteforce_login.py
+++ scenarios/dmz-lab/attacker_protocols/scripts/bruteforce_login.py
@@ -1,9 +1,9 @@
-import request
+import requests
 import os
 import argparse
 
 def argument_parse():
-    pars = argparse.ArgumentPraser(
+    pars = argparse.ArgumentParser(
             prog='Python Login Brute-Force',
             description='A small tool for brute-forcing a Login',
             usage='bruteforce_login.py -u [username file or single username] -p [password file] -t [target URL] --threads [number of threads (default 4)] -v [verbose mode(show each attempt)]',
@@ -18,26 +18,38 @@ def argument_parse():
 
     pars.add_argument(
             "-p", "--password",
-            requidred=True,
+            required=True,
             help="Path to the password file"
             )
 
     pars.add_argument(
             "-t", "--target",
+            required=True,
             help="URL of the target"
             )
     
     pars.add_argument(
             "--threads",
+            type=int,
+            default=4
             help="The Number of Threads"
             )
 
     pars.add_argument(
             "-v", "--verbose",
+            action"store_true",
             help="Verbose output"
             )
     return pars.parse_args()
 
 
+def brute_force(username_file, password_file, url):
+    try:
+        with open(password_file, "r") as password:
+            pass
+
+
+
 if __name__ == "__main__":
+    args = argument_parse()
 
