62d7bce2b7b6528e4bd2caa22913f88131131fb6
outercastl3 <outercastl3@proton.me>
2026-05-20 19:49:23 +0200

    fixed threading chunk problem

--- scenarios/dmz-lab/attacker_protocols/scripts/bruteforce_login_dvwa.py
+++ scenarios/dmz-lab/attacker_protocols/scripts/bruteforce_login_dvwa.py
@@ -87,7 +87,7 @@ def brute_force(username_file, password_file, num_threads, url, verbose):
     try:
         with open(password_file, "r") as f:
             passwords = f.readlines()
-            chunk_size = len(passwords) // num_threads)
+            chunk_size = max(1, len(passwords) // num_threads))
             chunks = [passwords[i:i + chunk_size] for i in range(0, len(passwords), chunk_size)]
 
         with open(username_file, "r") as f:
